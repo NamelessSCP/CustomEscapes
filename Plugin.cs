f6054c2b40d82cf82a44e9e04f6ee4c6c330cc35
Nameless <85962933+Misfiy@users.noreply.github.com>
2023-09-04 15:16:56 +0200

    update Plugin.cs

--- Plugin.cs
+++ Plugin.cs
@@ -1,48 +1,52 @@
 ﻿namespace CustomEscapes
 {
-     using Exiled.API.Features;
-     using Exiled.API.Enums;
-     using CustomEscapes.Events;
-     using Player = Exiled.Events.Handlers.Player;
+    using Exiled.API.Features;
+    using Exiled.API.Enums;
+    using CustomEscapes.Events;
+    using Player = Exiled.Events.Handlers.Player;
+    using System;
 
-     public class Escaping : Plugin<Config>
-     {
-          public override string Name => "CustomEscapes";
-          public override string Prefix => "CustomEscapes";
-          public override string Author => "@misfiy";
-          public override PluginPriority Priority => PluginPriority.Default;
-          private PlayerHandler playerHandler;
-          public static Escaping Instance;
-          private Config config;
-          public override void OnEnabled()
-          {
-               Instance = this;
-               config = Instance.Config;
+    public class Escaping : Plugin<Config>
+    {
+        public override string Name => "CustomEscapes";
+        public override string Prefix => "CustomEscapes";
+        public override string Author => "@misfiy";
+        public override PluginPriority Priority => PluginPriority.Default;
+        public override Version Version => new(1, 3, 2);
+        public override Version RequiredExiledVersion => new(8, 1, 0);
 
-               RegisterEvents();
-               base.OnEnabled();
-          }
+        private PlayerHandler playerHandler;
+        public static Escaping Instance;
+        private Config config;
+        public override void OnEnabled()
+        {
+            Instance = this;
+            config = Instance.Config;
 
-          public override void OnDisabled()
-          {
-               UnregisterEvents();
-               Instance = null!;
-               base.OnDisabled();
-          }
-          public void RegisterEvents()
-          {
-               playerHandler = new PlayerHandler();
-               Player.Escaping += playerHandler.OnEscaping;
-               Player.Spawned += playerHandler.OnSpawned;
+            RegisterEvents();
+            base.OnEnabled();
+        }
 
-               Log.Debug("Events have been registered!");
-          }
-          public void UnregisterEvents()
-          {
-               Player.Escaping -= playerHandler.OnEscaping;
-               Player.Spawned -= playerHandler.OnSpawned;
+        public override void OnDisabled()
+        {
+            UnregisterEvents();
+            Instance = null!;
+            base.OnDisabled();
+        }
+        public void RegisterEvents()
+        {
+            playerHandler = new PlayerHandler();
+            Player.Escaping += playerHandler.OnEscaping;
+            Player.Spawned += playerHandler.OnSpawned;
 
-               playerHandler = null!;
-          }
-     }
+            Log.Debug("Events have been registered!");
+        }
+        public void UnregisterEvents()
+        {
+            Player.Escaping -= playerHandler.OnEscaping;
+            Player.Spawned -= playerHandler.OnSpawned;
+
+            playerHandler = null!;
+        }
+    }
 }
